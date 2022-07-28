2244c60e73dade5554005829680764035d7763f1
VIJAY M S <VIJAY M S@DESKTOP-E68BUOQ>
2022-07-28 12:51:00 +0530

    FIRST COMMIT

---
+++ Firstprogram/src/MultiplacationTabel.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MultiplacationTabel {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please eneter number which you want to select");
+ 
+		int n =sc.nextInt();
+		for (int i=1;i<10;i++);
+		int i;
+		System.out.println(i+"X"+i+"="+i*);
+	}
+
+}
