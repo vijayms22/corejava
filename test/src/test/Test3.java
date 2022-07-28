23b4ec838b5f55ba9b3b9afaf96befc73e3a0428
VIJAY M S <VIJAY M S@DESKTOP-E68BUOQ>
2022-07-28 18:56:48 +0530

    FIRST Commit

---
+++ test/src/test/Test3.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=12;
+		while(x<10) {
+			x--;
+		}
+		System.out.println(x);
+		}
+
+	}
+
+
