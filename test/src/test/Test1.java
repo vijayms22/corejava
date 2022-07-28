81d0c683f20522a7a7ab255d444d1b3b80aeca5c
VIJAY M S <VIJAY M S@DESKTOP-E68BUOQ>
2022-07-28 18:48:59 +0530

    FIRST commit

---
+++ test/src/test/Test1.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Test1 {
+public static void main (String[] args) {
+	int a=5,b=6,c=7;
+	System.out.println("value is"+b+c);
+	System.out.println("a+b+c");
+	System.out.println("string"+(b+c));
+}
+}
