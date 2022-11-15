699568e58f4145a3f7efce895dcb6048bb401e00
VIJAY M S <VIJAY M S@172.30.16.153>
2022-11-15 14:19:50 +0530

    Pratian

--- Pratian/src/practice_lab/lab_4.java
+++ Pratian/src/practice_lab/lab_4.java
@@ -1,12 +1,31 @@
 package practice_lab;
-
+//import java.util.Scanner;
 public class lab_4 {
 
 	public static void main(String[] args) {
-	
+		
+			{
+			//	Scanner sc=new Scanner(System.in);
+				
+				int n=10,sum=0;
+				
+			//	System.out.println("Enter a n");
+				
+				//n=sc.nextInt();
+				
+				for(int i=1;i<=n;i++)
+				{
+					if(i%2!=0)
+					{
+						sum=sum+i;
+					}
+				}
+				System.out.println("The sum of odd no is "+sum);
+			}
+		}
 		
 		
 
 	}
 
-}
+
---
+++ Pratian/src/practice_lab/lab_6.java
@@ -0,0 +1,16 @@
+package practice_lab;
+
+public class lab_6 {
+
+	public static void main(String[] args) {
+		//str = str.toUpperCase();// for upper case
+		
+		String str =" hello world";
+		
+		
+		{str = str.toUpperCase();
+		
+				System.out.print(str);
+	}
+	}
+}
